957aedb16f39641e1e366053ed45705d56e4e089
Savio Moraes <smmsavio@hotmail.com>
2021-12-18 16:41:42 -0300

    modifica src e isntala dependencias

--- portfolio-savio/src/App.js
+++ portfolio-savio/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home, Perfil, NotFound } from './pages/';
+import { Header, Footer, Apresentation, Contatos } from './components';
+
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Apresentation />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/profile" element={ <Perfil /> } />
+        <Route path="*" element={ <NotFound /> } />
+      </Routes>
+      <Contatos />
+      <Footer />
+    </BrowserRouter>
   );
 }
 
