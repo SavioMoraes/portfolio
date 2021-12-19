d35add7694f2ae2b651db2d3433972c2c3ddb006
Savio Moraes <smmsavio@hotmail.com>
2021-12-19 14:34:35 -0300

    deleta app para configuração

--- portfolio-savio/src/pages/index.jsx
+++
@@ -1,3 +0,0 @@
-export { default as Home } from './home';
-export { default as Perfil } from './perfil';
-export { default as NotFound } from './notFound';
--- portfolio-savio/src/pages/notFound/index.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class NotFound extends Component {
-  render() {
-    return <div className="error">Página não encontrada</div>;
-  }
-}
-
-export default NotFound;
