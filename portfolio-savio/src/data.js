44bd51960a1251464d57eca35b5adacf55d0f193
Savio Moraes <smmsavio@hotmail.com>
2021-12-20 14:45:14 -0300

    altera names em data.js para redicionamento das paginas

--- portfolio-savio/src/data.js
+++ portfolio-savio/src/data.js
@@ -7,19 +7,19 @@ const projects = [
   },
   {
     title: "Pixels Art",
-    name: 'project-shopping-cart',
+    name: 'project-pixels-art',
     storyline: 'Nesse projeto, Pixels Art, criado com JavaScript, HTML e CSS, é gerado a cada refresh da página uma paleta com 4 cores rgb aleatoriamente! Também é possível escolher o tamanho do painel de pintura e apagá-lo quando necessário!',
     imagePath: 'images/pixels-art.png',
   },
   {
     title: 'To Do List',
-    name: 'project-shopping-cart',
+    name: 'project-to-do-list',
     storyline: 'O projeto To Do List foi criado com HTML, CSS e JavaScript e traz uma lista de tarefas, com posibilidade de adicionar tarefa, marcar como feita, definir prioridades e deleção de tarefas! Além disso, o botão de salvar tarefas utiliza o local storage!',
     imagePath: 'images/to-do-list.png',
   },
   {
     title: 'Movie Cards Library',
-    name: 'project-shopping-cart',
+    name: 'project-movie-cards-library',
     storyline: 'O projeto Movie Cards Library, feito em React e estilizado com CSS flex-box, traz uma biblioteca de filmes que são renderizados dinamicamente! São utilizados props na troca de informações entre os componentes e propTypes para checagem dos tipos de dados.',
     imagePath: 'images/movie-cards-library.png',
   },
