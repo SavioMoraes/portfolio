39ea334d708184d70a5ea39f0b13e295d8728090
Savio Moraes <smmsavio@hotmail.com>
2022-01-07 17:16:38 -0300

    add project movie card library CRUD

--- portfolio-savio/src/components/projectCard/index.jsx
+++ portfolio-savio/src/components/projectCard/index.jsx
@@ -7,23 +7,25 @@ import './projectCard.css'
 class ProjectCard extends React.Component {
   render() {
     const { project } = this.props;
-    const { title, name, storyline, imagePath } = project;
+    const { title, name, storyline, imagePath, url } = project;
     return (
-      <section className="project-card">
-        <a href={`https://saviomoraes.github.io/${name}/`} target="_blank" rel="noreferrer">
-          <img className="project-card-image" src={ imagePath } alt={ title } />
-        </a>
-        <div className="project-card-body">
-          <a href={`https://saviomoraes.github.io/${name}/`} target="_blank" rel="noreferrer">
-            <h4 className="project-card-title">{ title }</h4>
+      <>
+        <section className="project-card">
+          <a href={ name ? `https://saviomoraes.github.io/${name}/` : url} target="_blank" rel="noreferrer">
+            <img className="project-card-image" src={ imagePath } alt={ title } />
           </a>
-          <a className="link-repo" href={`https://github.com/SavioMoraes/${name}/`} target="_blank" rel="noreferrer">
-          <FontAwesomeIcon className="git-hub-icon" icon={faCodeBranch} />
-            <h5 className="project-card-git-hub">código github</h5>
-          </a>
-          <p className="project-card-storyline">{ storyline }</p>
-        </div>
-      </section>
+          <div className="project-card-body">
+            <a href={`https://saviomoraes.github.io/${name}/`} target="_blank" rel="noreferrer">
+              <h4 className="project-card-title">{ title }</h4>
+            </a>
+            <a className="link-repo" href={`https://github.com/SavioMoraes/${name}/`} target="_blank" rel="noreferrer">
+            <FontAwesomeIcon className="git-hub-icon" icon={faCodeBranch} />
+              <h5 className="project-card-git-hub">código github</h5>
+            </a>
+            <p className="project-card-storyline">{ storyline }</p>
+          </div>
+        </section>
+      </>
     )
   }
 }
--- portfolio-savio/src/data.js
+++ portfolio-savio/src/data.js
@@ -1,26 +1,32 @@
 const projects = [
   {
+    title: 'Movie Card Library CRUD',
+    url: 'https://project-movie-card-library-crud-flame.vercel.app/',
+    storyline: 'Feito em React, o projeto Movie Card Library CRUD, é uma bibliotaca de filmes onde é possível criar, visualizar, atualizar e deletar os filmes da biblioteca. Utiliza shouldComponentUpdate, componentDidUpdate, props.children, Rotas e Navegação. Estilizado com CSS responsivo.',
+    imagePath: 'images/movie-card-library-crud.png',
+  },
+  {
     title: 'Movie Cards Library',
     name: 'project-movie-cards-library',
-    storyline: 'O projeto Movie Cards Library, feito em React e estilizado com CSS flex-box, traz uma biblioteca de filmes que são renderizados dinamicamente! São utilizados props na troca de informações entre os componentes e propTypes para checagem dos tipos de dados.',
+    storyline: 'O projeto Movie Cards Library, feito em React e estilizado com CSS flex-box, traz uma biblioteca de filmes que são renderizados dinamicamente! São utilizados props na troca de informações entre os componentes e propTypes para checagem dos tipos de dados. Estilizado com CSS responsivo.',
     imagePath: 'images/movie-cards-library.png',
   },
   {
     title: 'To Do List',
     name: 'project-to-do-list',
-    storyline: 'O projeto To Do List foi criado com HTML, CSS e JavaScript e traz uma lista de tarefas, com posibilidade de adicionar tarefa, marcar como feita, definir prioridades e deleção de tarefas! Além disso, o botão de salvar tarefas utiliza o local storage!',
+    storyline: 'O projeto To Do List foi criado com HTML, CSS e JavaScript e traz uma lista de tarefas, com posibilidade de adicionar tarefa, marcar como feita, definir prioridades e deleção de tarefas! Além disso, o botão de salvar tarefas utiliza o local storage! Estilizado com CSS responsivo.',
     imagePath: 'images/to-do-list.png',
   },
   {
     title: "Pixels Art",
     name: 'project-pixels-art',
-    storyline: 'Nesse projeto, Pixels Art, criado com JavaScript, HTML e CSS, é gerado a cada refresh da página uma paleta com 5 cores rgb aleatoriamente! Também é possível escolher o tamanho do painel de pintura e apagá-lo quando necessário!',
+    storyline: 'Nesse projeto, Pixels Art, criado com JavaScript, HTML e CSS, é gerado a cada refresh da página uma paleta com 5 cores rgb aleatoriamente! Também é possível escolher o tamanho do painel de pintura e apagá-lo quando necessário! Estilizado com CSS responsivo.',
     imagePath: 'images/pixels-art.png',
   },
   {
     title: 'Shopping Cart',
     name: 'project-shopping-cart',
-    storyline: 'Projeto simulando um carrinho de compras, em JavaScript, fazendo requisição à API do Mercado Libre, para buscar os produtos, que ao serem selecionados, vão pro carrinho de compras, que calcula e exibe o valor total dinamicamente.',
+    storyline: 'Projeto simulando um carrinho de compras, em JavaScript, fazendo requisição à API do Mercado Libre, para buscar os produtos, que ao serem selecionados, vão pro carrinho de compras, que calcula e exibe o valor total dinamicamente. Estilizado com CSS responsivo.',
     imagePath: 'images/shopping-cart.png',
   },
 ];
